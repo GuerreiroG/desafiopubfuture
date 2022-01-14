c8d82a9add45020656189f420ffa829f3fe7a2a3
GuerreiroG <62075411+GuerreiroG@users.noreply.github.com>
2022-01-13 21:38:04 -0300

    Atualizado receita.py
    Foram feitas alterações no módulo receita.py, com correções feitas a partir dos resultados obtidos em testes.

--- receita.py
+++ receita.py
@@ -5,6 +5,7 @@ No arquivo encontra-se a implementação de uma classe: Receita
 Autor: Gustavo Guerreiro
 """
 import sqlite3
+import conta
 
 class Receita:
 
@@ -16,7 +17,7 @@ class Receita:
         self.data_recebimento_esperado = data_recebimento_esperado
         self.descricao = descricao
         self.conta = conta
-        self.tipo_receira = tipo_receita
+        self.tipo_receita = tipo_receita
 
     def criar_tabela_receita():
         conexao = sqlite3.connect("controleFinancas.bd")
@@ -24,14 +25,14 @@ class Receita:
         cursor = conexao.cursor()
         cursor.execute("""
         create table if not exists receita
-        (id_receita integer primary key,
+        (idReceita integer primary key,
         valor real not null,
         dataRecebimento date,
         dataRecebimentoEsperado date,
         descricao text,
         conta integer not null,
         tipoReceita text,
-        foreign key (conta) references conta (id_conta))
+        foreign key (conta) references conta (idConta))
         """)
         conexao.commit()
         conexao.close()
@@ -39,21 +40,26 @@ class Receita:
     def salvar_receita(self):
         receita = (self.valor, self.data_recebimento, 
         self.data_recebimento_esperado, self.descricao, self.conta, 
-        self.tipo_receira)
+        self.tipo_receita)
         conexao = sqlite3.connect("controleFinancas.bd")
         conexao.execute("PRAGMA foreign_keys = 1")
         cursor = conexao.cursor()
+        print(receita)
         cursor.execute("""insert into receita values (null, ?, ?, ?, ?, ?, ?)
         """, receita)
+        cursor.execute("select saldo + ? from conta where idConta = ?", 
+        (self.valor, self.conta))
+        novo_saldo = cursor.fetchone()[0]
         conexao.commit()
         conexao.close()
+        conta.Conta.editar_conta(self.conta, 1, novo_saldo)
 
     def editar_receita(id_receita, opcao, valor):
-        opcoes = {1:"valor", 2:"dataRecebimento", 3:"dataRecebimentoEsperado",
-        4:"descricao", 5:"conta", 6:"tipoReceita"}
+        opcoes = {1:"dataRecebimento", 2:"dataRecebimentoEsperado",
+        3:"descricao", 4:"conta", 5:"tipoReceita"}
         conexao = sqlite3.connect("controleFinancas.bd")
         cursor = conexao.cursor()
-        receita = cursor.execute("select * from conta where idReceita = ?", 
+        receita = cursor.execute("select * from receita where idReceita = ?", 
         (id_receita, )).fetchone()
         if receita is None:
             print("Receita inexistente!")
@@ -66,12 +72,12 @@ class Receita:
     def remover_receitas(id_receita):
         conexao = sqlite3.connect("controleFinancas.bd")
         cursor = conexao.cursor()
-        receita = cursor.execute("select * from conta where idReceita = ?", 
+        receita = cursor.execute("select * from receita where idReceita = ?", 
         (id_receita, )).fetchone()
         if receita is None:
             print("Conta inexistente!")
         else:
-            cursor.execute("delete from conta where idReceita = ?",
+            cursor.execute("delete from receita where idReceita = ?",
             (id_receita, ))
         conexao.commit()
         conexao.close()
@@ -80,15 +86,15 @@ class Receita:
         conexao = sqlite3.connect("controleFinancas.bd")
         cursor = conexao.cursor()
         match filtro:
-            case "1":
+            case 1:
                 cursor.execute("select * from receita where dataRecebimento \
                 between ? and ?", (valor['data_inicial'], valor['data_final'])) 
                 receitas = cursor.fetchall()
                 print(receitas)
-            case "2":
+            case 2:
                 cursor.execute("select * from receita where tipoReceita = ?", 
-                (valor['tipo_receita']))
-
+                (valor['tipo_receita'], ))
+                print(cursor.fetchall())
         conexao.commit()
         conexao.close()
 
@@ -101,3 +107,34 @@ class Receita:
         conexao.close()
         for receita in receitas:
             print(receita)
+
+if __name__ == "__main__":
+    opcao = None
+    while opcao != "0":
+        opcao = input("Digite o número correspondente à função: ")
+        match opcao:
+            case "1":
+                Receita.criar_tabela_receita()
+                receita1 = Receita(20, '2022-01-05', '2022-01-05', 'descrição',
+                1, 'tipo')
+                receita2 = Receita(20, '2022-01-10', '2022-01-07', 'descrição2',
+                2, 'tipo2')
+                receita3 = Receita(20, '2022-01-10', '2022-01-07', 'descrição3',
+                2, 'tipo3')
+                receita1.salvar_receita()
+                receita2.salvar_receita()
+                receita3.salvar_receita()
+            case "2":
+                Receita.editar_receita(1,1,'2021-01-04')
+            case "3":
+                Receita.remover_receitas(3)
+            case "4":
+                selecao = input("Digite '1' para seleção por data e '2' para"
+                + "seleção por tipo.")
+                match selecao:
+                    case "1":
+                        Receita.listar_receitas_filtro(1, data_inicial = '2022-01-07', data_final = '2022-01-12')
+                    case "2":
+                        Receita.listar_receitas_filtro(2, tipo_receita = "tipo")
+            case "5":
+                Receita.listar_receitas_total()
